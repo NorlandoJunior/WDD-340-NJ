c9fe2fd69dc1912d6845d1528cccbf1c15938d5a
Norlando_jr <norlandojr@hotmail.com>
2025-11-12 17:50:07 -0300

    Robust Error Handling

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.set("layout", "./layouts/layout"); // not at views root
  *************************/
 app.use(express.static("public"))
 app.use(static)
-//index route
-app.get("/", baseController.buildHome);
+// Index route
+app.get("/", utilities.handleErrors(baseController.buildHome));
 // Inventory route
 app.use("/inv", inventoryRoute);
 // File Not Found Route - must be last route in list
@@ -47,9 +47,10 @@ const host = process.env.HOST
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
+    message,
     nav
   })
 })
