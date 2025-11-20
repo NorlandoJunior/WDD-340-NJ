119dc1b70ea344c992d8a609766db465aeac6e6d
Norlando_jr <norlandojr@hotmail.com>
2025-11-19 22:04:13 -0300

    Password Hashing

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,6 @@
 const utilities = require("../utilities/")
 const accountModel = require("../models/account-model")
+const bcrypt = require("bcryptjs")
 
 /* ****************************************
 *  Deliver login view
@@ -59,11 +60,25 @@ async function registerAccount(req, res, next) {
     })
   }
 
-  const regResult = await accountModel.registerAccount(
+  // Hash the password before storing
+  let hashedPassword
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hash(account_password, 10)
+  } catch (error) {
+    req.flash("notice", 'Sorry, there was an error processing the registration.')
+    res.status(500).render("account/register", {
+      title: "Registration",
+      nav,
+      errors: null,
+    })
+  }
+
+const regResult = await accountModel.registerAccount(
     account_firstname,
     account_lastname,
     account_email,
-    account_password
+    hashedPassword
   )
 
   if (regResult && regResult.rowCount > 0) {
